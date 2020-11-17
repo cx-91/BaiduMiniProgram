a9e4398c39ed72dedd0147686d78d34ce0b5799b
ChanghanXie <ylbbak99@163.com>
2020-11-17 03:21:03 -0500

    update tabbar wen zhang

--- moodBank/utils/status_mock.js
+++ moodBank/utils/status_mock.js
@@ -7,135 +7,40 @@ export const feedList =  [{
     theme: 'default',
     content: {
         idx: '0',
-        title: '宠物自己在家时, 如何帮助它度过孤独时光',
-        infoSource: '萌宠在家',
-        commentsNum: 183,
+        title: '颜色和情绪：颜色怎样影响你感受',
+        infoSource: 'Allison S. Gremillion',
         images: ['/images/status_img/demo1.jpg']
     }
 }, {
     theme: 'default',
     content: {
         idx: '1',
-        title: '海洋的最深处到底有什么神奇的生物？',
-        infoSource: '智慧百科',
-        commentsNum: 183,
+        title: '颜色对人体生理和心理的影响',
+        infoSource: '中国健康心理学杂志',
         images: ['/images/status_img/demo2.jpg']
-    },
-    video: {
-        isVideo: true,
-        time: '05:20'
-    }
-// }, {
-//     theme: 'multiple-images',
-//     content: {
-//         title: '欧洲各地被遗忘的地方，似乎时间被施了魔法一样，一切都已经停止了',
-//         infoSource: '旅行游记',
-//         commentsNum: '18.3万',
-//         images: [
-//             '/@smt-ui-template-feed/images/demo3.jpg',
-//             '/@smt-ui-template-feed/images/demo4.jpg',
-//             '/@smt-ui-template-feed/images/demo2.jpg']
-//     }
-// }, {
-//     theme: 'large-image',
-//     content: {
-//         title: '跟猫主子过圣诞节要什么男朋友，单身朋友的福音',
-//         infoSource: '萌宠在家',
-//         commentsNum: '18.3万',
-//         images: ['/@smt-ui-template-feed/images/demo6.jpg']
-//     }
-// }, {
-//     theme: 'large-image',
-//     content: {
-//         title: '跟猫主子过圣诞节要什么男朋友，单身朋友的福音',
-//         infoSource: '萌宠在家',
-//         commentsNum: '18.3万',
-//         images: ['/@smt-ui-template-feed/images/demo7.jpg']
-//     },
-//     video: {
-//         isVideo: true,
-//         time: '05:20'
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '2020装修流行趋势指南，小户型也有大空间，简单实用',
-//         infoSource: '实用家装',
-//         commentsNum: '18.3万'
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '宠物自己在家时, 如何帮助它度过孤独时光',
-//         infoSource: '萌宠在家',
-//         commentsNum: 183,
-//         images: ['/@smt-ui-template-feed/images/demo1.jpg']
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '2020装修流行趋势指南，小户型也有大空间，简单实用',
-//         infoSource: '实用家装',
-//         commentsNum: '18.3万'
-//     }
-// }, {
-//     theme: 'large-image',
-//     content: {
-//         title: '跟猫主子过圣诞节要什么男朋友，单身朋友的福音',
-//         infoSource: '萌宠在家',
-//         commentsNum: '18.3万',
-//         images: ['/@smt-ui-template-feed/images/demo7.jpg']
-//     },
-//     video: {
-//         isVideo: true,
-//         time: '05:20'
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '宠物自己在家时, 如何帮助它度过孤独时光',
-//         infoSource: '萌宠在家',
-//         commentsNum: 183,
-//         images: ['/@smt-ui-template-feed/images/demo1.jpg']
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '宠物自己在家时, 如何帮助它度过孤独时光',
-//         infoSource: '萌宠在家',
-//         commentsNum: 183,
-//         images: ['/@smt-ui-template-feed/images/demo1.jpg']
-//     }
-// }, {
-//     theme: 'default',
-//     content: {
-//         title: '海洋的最深处到底有什么神奇的生物？',
-//         infoSource: '智慧百科',
-//         commentsNum: 183,
-//         images: ['/@smt-ui-template-feed/images/demo2.jpg']
-//     },
-//     video: {
-//         isVideo: true,
-//         time: '05:20'
-//     }
-// }, {
-//     theme: 'large-image',
-//     content: {
-//         title: '跟猫主子过圣诞节要什么男朋友，单身朋友的福音',
-//         infoSource: '萌宠在家',
-//         commentsNum: '18.3万',
-//         images: ['/@smt-ui-template-feed/images/demo6.jpg']
-//     }
-// }, {
-//     theme: 'large-image',
-//     content: {
-//         title: '跟猫主子过圣诞节要什么男朋友，单身朋友的福音',
-//         infoSource: '萌宠在家',
-//         commentsNum: '18.3万',
-//         images: ['/@smt-ui-template-feed/images/demo7.jpg']
-//     },
-//     video: {
-//         isVideo: true,
-//         time: '05:20'
-//     }
-}];
+    }},
+    {
+    theme: 'default',
+    content: {
+        idx: '2',
+        title: '颜色知觉对心理功能的影响',
+        infoSource: '黄辛隐、王富',
+        images: ['/images/status_img/demo3.jpg']
+        }},
+    {
+    theme: 'default',
+    content: {
+        idx: '3',
+        title: '别担心，开心点',
+        infoSource: 'Maria Konnikova',
+        images: ['/images/status_img/demo4.jpg']
+            }},
+    {
+    theme: 'default',
+    content: {
+        idx: '4',
+        title: '大学成抑郁症重灾区，大一是坎大三是坑',
+        infoSource: '中国新闻周刊',
+        images: ['/images/status_img/demo5.jpg']
+                }}
+];
